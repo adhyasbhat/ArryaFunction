c69fd18a7a05f5a559366a2d5ad7ba413609cf02
adhyasbhat <adhyasbhat@gmail.com>
2023-07-07 06:20:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Task } from './app.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ArryaFunctions';
+
+  tasks: Task[] = [
+    new Task(1, 'Task 1', true),
+    new Task(2, 'Task 2', false),
+    new Task(3, 'Task 3', true),
+    new Task(4, 'Task 4', false),
+    new Task(5, 'Task 5', true)
+  ];
+
+  filteredTasks: Task[] = this.tasks.filter(task => task.name.includes('Task'));
+
+  completedTasks: Task[] = this.tasks.filter(task => task.completed);
+
+  taskNames: string[] = this.tasks.map(task => task.name);
+
+  taskCount: number = this.tasks.reduce((count, task) => count + 1, 0);
+
+  taskNamesConcatenated: string = this.tasks.reduce((concatenated, task) => concatenated + task.name + ', ', '');
+
+  taskWithId3: Task | undefined = this.tasks.find(task => task.id === 3);
+
+  areAllTasksCompleted: boolean = this.tasks.every(task => task.completed);
+
+  hasIncompleteTask: boolean = this.tasks.some(task => !task.completed);
 }
